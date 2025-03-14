853b036fc240d3f2d14a920e63868649635279e1
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-03-14 13:16:13 +0900

    test : TestHeight
    TestHeight 추가 및 주석 제거

--- Source/TopDownPortfolio/Private/C_MakeHeightMap.cpp
+++ Source/TopDownPortfolio/Private/C_MakeHeightMap.cpp
@@ -27,64 +27,6 @@ void UC_MakeHeightMap::PostEditChangeProperty(FPropertyChangedEvent& PropertyCha
 	UActorComponent::PostEditChangeProperty(PropertyChangedEvent);
 	if (m_bMakeTexture)
 	{
-		//if (ULandscapeInfo* LandscapeInfo = m_pLandscapeInfo)
-		//{
-		//	FIntRect LandscapeExtent;
-		//	LandscapeInfo->GetLandscapeExtent(LandscapeExtent);
-
-		//	LandscapeExtent.Max.X += 1;
-		//	LandscapeExtent.Max.Y += 1;
-
-		//	FGuid CurrentLayerGuid = m_pLandscape->GetGridGuid();
-		//	//	guid->GetCurrentLayerGuid();
-
-		//	const ELandscapeLayerPaintingRestriction PaintRestriction = ELandscapeLayerPaintingRestriction::None;
-
-		//	ELandscapeImportTransformType TransformType = ELandscapeImportTransformType::ExpandOffset; //  LandscapeEdMode->UISettings->ImportType;
-
-		//	FVector LocalGizmoPosition = LandscapeInfo->GetLandscapeProxy()->LandscapeActorToWorld().InverseTransformPosition(m_pLandscape->GetActorLocation());
-		//	FIntPoint LocalGizmoPoint = FIntPoint(FMath::FloorToInt32(LocalGizmoPosition.X), FMath::FloorToInt32(LocalGizmoPosition.Y));
-
-		//	// Update Gizmo Position if we exit and comeback into tool
-		//	//LandscapeEdMode->UISettings->ImportLandscape_GizmoLocalPosition = LocalGizmoPoint;
-
-		//	FIntRect ImportRegion = LandscapeExtent;
-		//	FIntPoint ImportOffset(0, 0);
-		//	if (TransformType == ELandscapeImportTransformType::ExpandOffset)
-		//	{
-		//		ImportOffset = LocalGizmoPoint - FIntPoint(LandscapeExtent.Min.X, LandscapeExtent.Min.Y);
-		//	}
-		//	else if (TransformType == ELandscapeImportTransformType::None)
-		//	{
-		//		ImportRegion = FIntRect(
-		//			LocalGizmoPoint.X,
-		//			LocalGizmoPoint.Y,
-		//			LocalGizmoPoint.X + LandscapeExtent.Max.X,
-		//			LocalGizmoPoint.Y + LandscapeExtent.Max.Y);
-		//	}
-		//	#if WITH_EDITOR
-		//	FEdModeLandscape* LandscapeEdMode = (FEdModeLandscape*)GLevelEditorModeTools().GetActiveMode(FBuiltinEditorModes::EM_Landscape);;
-		//	//if (!m_LoadHeight.Get())
-		//	//	m_LoadHeight.LoadSynchronous();
-		//	//m_LoadHeight.Get()->GetDefaultConfigFilename()
-		//	if (LandscapeEdMode )// && m_LoadHeight.Get())
-		//	{
-		//		//LandscapeEdMode->ImportHeightData(LandscapeInfo, CurrentLayerGuid,
-		//		//	m_LoadHeight, LandscapeExtent,
-		//		//	TransformType, ImportOffset, PaintRestriction, LandscapeEdMode->UISettings->bFlipYAxis);
-		//	}
-		//	#endif
-		//		//ImportDataInternal<uint16>(LandscapeInfo, FilePath, NAME_None, true, false, ImportRegion, TransformType, ImportOffset, [LandscapeInfo, CurrentLayerGuid, PaintRestriction](int32 MinX, int32 MinY, int32 MaxX, int32 MaxY, const TArray<uint16>& Data)
-		//	//	{
-		//	//		ALandscape* Landscape = LandscapeInfo->LandscapeActor.Get();
-		//	//		FScopedSetLandscapeEditingLayer Scope(Landscape, CurrentLayerGuid, [&] { check(Landscape); Landscape->RequestLayersContentUpdate(ELandscapeLayerUpdateMode::Update_Heightmap_All); });
-		//	//
-		//	//		//FScopedTransaction Transaction(LOCTEXT("Undo_ImportHeightmap", "Importing Landscape Heightmap"));
-		//	//
-		//	//		FHeightmapAccessor<false> HeightmapAccessor(LandscapeInfo);
-		//	//		HeightmapAccessor.SetData(MinX, MinY, MaxX, MaxY, Data.GetData());
-		//	//	});
-		//}
 			m_nCount = 0;
 			m_bMakeTexture = false;
 	}
@@ -121,7 +63,6 @@ void UC_MakeHeightMap::E_GenerateHeightMap()
 	FLandscapeEditDataInterface LandscapeEdit{ m_pLandscapeInfo };
 	FIntRect fData{};
 	m_pLandscapeInfo->GetLandscapeExtent(fData);
-	//LandscapeEdit.GetHeightDataFast(fData.Min.X, fData.Min.Y, fData.Max.X, fData.Max.Y, m_arHeightData.GetData(), m_nStride);
 	m_fAlpha = m_fLandScapeSize.Max.X - m_fLandScapeSize.Min.X;
 	E_Logic(m_fLandScapeSize.Max.X - m_fLandScapeSize.Min.X);
 	for (ULandscapeComponent* Component : m_pLandscape->LandscapeComponents)
@@ -139,30 +80,7 @@ void UC_MakeHeightMap::E_UpdateLandscape()
 {
 	if (!m_pLandscape || !m_pLandscapeInfo)
 		return;
-	//for (ULandscapeComponent* Component : m_pLandscape->LandscapeComponents)
-	//{
-	//	if (Component)
-	//	{
-	//		Component->MarkRenderStateDirty();
-	//		Component->RequestHeightmapUpdate(true, true);
-	//		Component->UpdateCachedBounds(true);
-	//		Component->UpdateNavigationBounds();
-	//		Component->PostLoad();
-	//	}
-	//}
-	//
-	//for (ULandscapeHeightfieldCollisionComponent* Component : m_pLandscape->CollisionComponents)
-	//{
-	//	if (Component)
-	//	{
-	//		Component->RecreateCollision();
-	//		Component->RecreateRenderState_Concurrent();
-	//	}
-	//}
-	//m_pLandscape->FlushGrassComponents();
-	//m_pLandscape->RecreateCollisionComponents();
 	m_pLandscapeInfo->ForceLayersFullUpdate();
-	//m_pLandscapeInfo->Reset();
 }
 
 void UC_MakeHeightMap::E_ExportHeightmapToPNG()
@@ -203,7 +121,6 @@ int UC_MakeHeightMap::E_Logic(int nDist)
 			E_CalCul(i, j, m_nSX, m_nSY, Half);
 		}
 	}
-	//m_fAlpha *= m_fFactor;
 	return 1 + E_Logic(Half);
 }
 
@@ -227,7 +144,7 @@ void UC_MakeHeightMap::E_CalCul(int X, int Y, const int* dX, const int* dY, uint
 		return;
 	*pCurrent = nAVG / nCount;
 	float Height = E_GetRoughnessFactor(S);
-	*pCurrent += Height; // LandscapeDataAccess::GetTexHeight(Height);
+	*pCurrent += Height; 
 }
 
 int32 UC_MakeHeightMap::E_GetMapSize()
